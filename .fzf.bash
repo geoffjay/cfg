6278c8986c5bb8b228961da66fb9a902f1d304f5
Geoff Johnson <geoff.jay@gmail.com>
2024-02-26 12:26:22 -0800

    chore: add missing dotfiles

---
+++ .fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/geoff/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/geoff/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/geoff/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/geoff/.fzf/shell/key-bindings.bash"
