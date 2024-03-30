f12e334a53b67837bfbfe4c91cdbab951e120aa0
Geoff Johnson <geoff.jay@gmail.com>
2024-03-29 20:56:31 -0700

    feat: tweak for nixos

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,38 +1,42 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "geoff";
-  home.homeDirectory = "/Users/geoff";
+  home.username = "cap";
+  home.homeDirectory = "/home/cap";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs; [
-    # It is sometimes useful to fine-tune packages, for example, by applying
-    # overrides. You can do that directly here, just don't forget the
-    # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # fonts?
+  home.packages = [
+    pkgs._1password
+    pkgs._1password-gui
+    pkgs.babashka
+    pkgs.clang
+    pkgs.curl
+    pkgs.dejavu_fonts
+    pkgs.kitty
+    pkgs.nerdfonts
+    pkgs.powerline-fonts
+    pkgs.starship
+    pkgs.tmux
+    pkgs.wget
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # You can also create simple shell scripts directly inside your
-    # configuration. For example, this adds a command 'my-hello' to your
-    # environment:
-    (writeShellScriptBin "test-hello" ''
-      echo "Hello, ${config.home.username}!"
-    '')
-
-    direnv
-    glab
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -50,20 +54,63 @@
     # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
   #
   #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   #
+      # Installations with additional options. For the list of options, please refer to Zplug README.
   # or
   #
-  #  /etc/profiles/per-user/geoff/etc/profile.d/hm-session-vars.sh
+  #  /etc/profiles/per-user/cap/etc/profile.d/hm-session-vars.sh
   #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     # EDITOR = "emacs";
+    CFGDIR = "$HOME/.cfg/";
   };
+
+  fonts.fontconfig.enable = true;
+
+  # fonts.packages = with pkgs; [
+  #   noto-fonts
+  #   noto-fonts-cjk
+  #   noto-fonts-emoji
+  #   liberation_ttf
+  #   fira-code
+  #   fira-code-symbols
+  #   mplus-outline-fonts.githubRelease
+  #   dina-font
+  #   proggyfonts
+  # ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Geoff Johnson";
+    userEmail = "geoff.jay@gmail.com";
+  };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+      hm-update = "NIXPKGS_ALLOW_UNFREE=1 home-manager switch";
+      g = "git";
+      v = "vim";
+      cfg = "/home/cap/.nix-profile/bin/git --git-dir=$CFGDIR --work-tree=$HOME";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+      ];
+    };
+  };
 }
